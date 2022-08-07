272ad1d4e1887b06d49327fc1e9ea5784775c7a7
Darragh ORiordan <darragh.oriordan@gmail.com>
2022-08-07 23:02:32 +1000

    update docs

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
-starting
+# What is this?
+
+A learning tool and demo for building microservies with good dev experience.
+
+There's apps in .net, react and nodejs. There's instances of prometheus, envoy, grafana, dapr, seq, zipkin, alertmanager.
+
+It's all local in docker compose which is nice for dev experience imho.
+
+There's localstack, local dynamo and a bunch of other service communication and pubsub services for dependencies.
+
+# Building
+
+rename dapr-secrets-local.json.template to dapr-secrets-local.json - if you have any issues with aws you might need a real aws secret key
+
+rename any .env.example to .env
+
+```bash
+docker compose build
+docker compose up
+```
+
+# starting
 
 note that on FIRST (only first) init the database takes a while to init so wait a minute or two then `docker compose down` and `docker compose up` again
+
+occasionaly the products-api gets into trouble in the same way - just down a `down` and `up` an it should be ok.
+
+There are many services that expose many ports to the host for debugging, these may cause conflicts with other running apps or containers
+
+# Grafana
+
+needs to be told where to find prometheus
+
+new datasource > prometheus > source = http://prometheus:9090 , name = Dapr, default = True
+then add the dashboards from /grafana-meta/dapr
--- packages/products-api/src/core-logger/OpenTelemetry.ts
+++ packages/products-api/src/core-logger/OpenTelemetry.ts
@@ -1,31 +1,26 @@
-//import {ZipkinExporter} from "@opentelemetry/exporter-zipkin";
-//import {registerInstrumentations} from "@opentelemetry/instrumentation";
 import {W3CTraceContextPropagator} from "@opentelemetry/core";
 import {NodeTracerProvider} from "@opentelemetry/sdk-trace-node";
 import {BatchSpanProcessor} from "@opentelemetry/sdk-trace-base";
 import {registerInstrumentations} from "@opentelemetry/instrumentation";
 import {diag, DiagConsoleLogger, DiagLogLevel} from "@opentelemetry/api";
 import {Resource} from "@opentelemetry/resources";
-//import {SemanticResourceAttributes} from "@opentelemetry/semantic-conventions";
 import {getNodeAutoInstrumentations} from "@opentelemetry/auto-instrumentations-node";
 import {SemanticResourceAttributes} from "@opentelemetry/semantic-conventions";
-import {HttpInstrumentation} from "@opentelemetry/instrumentation-http";
 import {OTLPTraceExporter} from "@opentelemetry/exporter-trace-otlp-http";
 
+// Set an internal logger for open telemetry to report any issues to your console/stdout
 diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.WARN);
+
 export const initTelemetry = (config: {
     appName: string;
     telemetryUrl: string;
 }): void => {
+    // create an exporter to an open telemetry exporter. We create this collector instance locally using docker compose.
     const exporter = new OTLPTraceExporter({
-        // optional - url default value is http://localhost:4318/v1/traces
-        url: config.telemetryUrl, // "http://otel-collector:4318/v1/traces",
+        url: config.telemetryUrl, // e.g. "http://otel-collector:4318/v1/traces",
     });
-    // const exporter = new ZipkinExporter({
-    //     serviceName: "zipkin instance",
-    //     url: config.telemetryUrl,
-    // });
 
+    // We add some common meta data to every trace. The service name is important.
     const resource = Resource.default().merge(
         new Resource({
             [SemanticResourceAttributes.SERVICE_NAME]: config.appName,
@@ -33,30 +28,20 @@ export const initTelemetry = (config: {
         })
     );
 
+    // We use the node trace provider provided by open telemetry
     const provider = new NodeTracerProvider({resource});
+
+    // The batch span provider is more efficient than the basic provider. This will batch sends to
+    // the exporter you have configured
     provider.addSpanProcessor(new BatchSpanProcessor(exporter));
 
-    // Initialize the provider - context manager default is OK
+    // Initialize the propagator
     provider.register({
         propagator: new W3CTraceContextPropagator(),
     });
 
     // Registering instrumentations / plugins
     registerInstrumentations({
-        instrumentations: getNodeAutoInstrumentations({
-            "@opentelemetry/instrumentation-http": {enabled: false},
-        }).concat(
-            // we want to ignore seq so load this in a special way
-            new HttpInstrumentation({
-                enabled: true,
-                ignoreOutgoingRequestHook: (req) => {
-                    return (
-                        (req && req.hostname?.includes("seq")) ||
-                        req.host?.includes("seq") ||
-                        false
-                    );
-                },
-            })
-        ),
+        instrumentations: getNodeAutoInstrumentations(),
     });
 };
